c691ae5e56789558e263d87e288b9f923ac36dc8
webabdulla <webabdullah7410@gmail.com>
2023-05-19 17:12:16 +0600

    tabs added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "../Banner/Banner";
 import Carousel from "../Carousel/Carousel";
 import Special from "../Special/Special";
+import FillExample from "../Tabs/FillExample";
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Carousel></Carousel>
+            <FillExample></FillExample>
            <Special></Special>
         </div>
     );
