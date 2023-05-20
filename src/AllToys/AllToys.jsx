0342719c064505fd5f89a48d0dc9366e4a6f8499
webabdulla <webabdullah7410@gmail.com>
2023-05-21 05:41:52 +0600

    all toys updated

--- src/AllToys/AllToys.jsx
+++ src/AllToys/AllToys.jsx
@@ -1,28 +1,56 @@
-import  { useEffect, useState } from "react";
+import  { useEffect, useState } from 'react';
+import './AllToy.css'
+import { Link } from 'react-router-dom';
 
 const ToysTable = () => {
   const [toys, setToys] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [showAll, setShowAll] = useState(false);
 
   useEffect(() => {
-    fetch("http://localhost:5000/toys")
-      .then((response) => response.json())
-      .then((toysData) => {
+    fetch('http://localhost:5000/toys')
+      .then(response => response.json())
+      .then(toysData => {
         setToys(toysData.slice(0, 20));
       })
-      .catch((error) => {
-        console.error("Error:", error);
+      .catch(error => {
+        console.error('Error:', error);
       });
   }, []);
 
+  const handleSearchChange = event => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleShowAll = () => {
+    setShowAll(true);
+  };
+
+  const filteredToys = showAll
+    ? toys
+    : toys.filter(toy =>
+        toy.name.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+
   return (
     <div>
-      <div className="text-center mb-16">
-        <h2 className="text-3xl font-bold">Marvel Toys</h2>
-        <p className="font-bold">
-          Your Destination for Marvel Superhero Collectibles
+      <div className='text-center'>
+        <h2 className='text-2xl font-bold'>Marvel Toys</h2>
+        <p className='font-bold'>
+          Marvel Universe Collectibles: Assemble Your Epic Collection!
         </p>
       </div>
-      <table>
+      <div className='mx-auto w-72'>
+        <input
+          type='text'
+          placeholder='Search by Toy Name'
+          value={searchQuery}
+          onChange={handleSearchChange}
+          className='search'
+        />
+      </div>
+
+      <table className='toys-table'>
         <thead>
           <tr>
             <th>Seller</th>
@@ -34,26 +62,30 @@ const ToysTable = () => {
           </tr>
         </thead>
         <tbody>
-          {toys.map((toy) => (
+          {filteredToys.map(toy => (
             <tr key={toy._id}>
-              <td>{toy.sellerName || ""}</td>
+              <td>{toy.sellerName || ''}</td>
               <td>{toy.name}</td>
               <td>{toy.category}</td>
               <td>{toy.price}</td>
               <td>{toy.quantity}</td>
               <td>
-                <button
-                  onClick={() =>
-                    console.log("View Details clicked for toy:", toy)
-                  }
-                >
-                  View Details
-                </button>
+                <Link to={`/details/${toy._id}`}>
+                  <button onClick={() => console.log('View Details clicked for toy:', toy)}>
+                    View Details
+                  </button>
+                </Link>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
+
+      <div className='show-all-container'>
+        <button onClick={handleShowAll} className='show-all-btn'>
+          Show All
+        </button>
+      </div>
     </div>
   );
 };
